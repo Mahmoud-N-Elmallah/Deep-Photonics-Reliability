2f420b7cb552429264eaba56cbc5cec02649e20a
Mahmoud N. Elmallah <mahmoudelmalah85@gamil.com>
2026-04-09 16:14:06 +0200

    add data path to config.py

--- src/config.py
+++ src/config.py
@@ -3,3 +3,6 @@ TRAIN_STD = 0.182
 NOTCH_WIDTH = 5
 NOTCH_DEPTH = 0.95
 APPLY_BILATERAL = False
+TRAIN_DATA_PATH='../data/train_data.csv'
+TEST_DATA_PATH='../data/test_data.csv'
+VAL_DATA_PATH='../data/val_data.csv'

6dd42a5e54e5a427f8662ebee2613bf5e9cb2af7
Mahmoud N. Elmallah <mahmoudelmalah85@gamil.com>
2026-04-09 20:51:33 +0200

    refactoring the src file

--- src/train.py
+++ src/train.py
@@ -6,11 +6,10 @@ import torch.nn as nn
 import torch
 
 def main():
-    config = load_config('src/config.yaml')
+    config = load_config('../src/config.yaml')
     device = setup_device()
     
     # Inject computed weights into config
-    config['class_weight'] = compute_class_weights(config)
     
     train_loader, val_loader,test_loader = build_loaders(config)
     model = PhotonicResNet50().to(device)

8059e4c8edab301555065becd2c05d2a9a275a90
Mahmoud N. Elmallah <mahmoudelmalah85@gamil.com>
2026-04-09 16:34:09 +0200

    create cofig.yaml

--- src/config.py
+++
@@ -1,8 +0,0 @@
-TRAIN_MEAN = 0.627
-TRAIN_STD = 0.182
-NOTCH_WIDTH = 5
-NOTCH_DEPTH = 0.95
-APPLY_BILATERAL = False
-TRAIN_DATA_PATH='../data/train_data.csv'
-TEST_DATA_PATH='../data/test_data.csv'
-VAL_DATA_PATH='../data/val_data.csv'

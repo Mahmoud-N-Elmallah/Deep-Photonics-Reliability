bac16059b27422766c6a6d5c07aea161eb1ac3d5
Mahmoud N. Elmallah <mahmoudelmalah85@gamil.com>
2026-04-10 10:37:47 +0200

    add more transformations to the data

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -9,25 +9,8 @@ from dataset import *
 
 def build_loaders(config: Dict, project_root: Path = None):
 
-    # 1. Transforms
-    train_transform = transforms.Compose([
-    FftTransform(
-        width=config['fft_params']['notch_width'], 
-        notch_depth=config['fft_params']['notch_depth'],
-        apply_bilateral=config['fft_params']['apply_bilateral'],dual_channel=config['fft_params']['dual_channel']),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomVerticalFlip(p=0.5),
-    # Rotations (90 deg increase) to not destroy the fft transform
-    transforms.RandomChoice([
-        transforms.RandomRotation((0, 0)),
-        transforms.RandomRotation((90, 90)),
-        transforms.RandomRotation((180, 180)),
-        transforms.RandomRotation((270, 270))
-    ]),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[config['stats']['train_original_mean'],config['stats']['train_fft_mean']],
-                        std=[config['stats']['train_original_std'],config['stats']['train_fft_std']])])
-   
+    # 1 Transforms
+    
     train_transform = transforms.Compose([
     v2.RandomHorizontalFlip(p=0.5),
     v2.RandomVerticalFlip(p=0.5),
@@ -49,27 +32,29 @@ def build_loaders(config: Dict, project_root: Path = None):
         mean=[config['stats']['train_original_mean'], config['stats']['train_fft_mean']],
         std=[config['stats']['train_original_std'], config['stats']['train_fft_std']])])
    
-   
-   
-   
-   
-    val_transform =transforms.Compose([FftTransform(width=config['fft_params']['notch_width'], notch_depth=config['fft_params']['notch_depth'],
-                                                    apply_bilateral=config['fft_params']['apply_bilateral'],dual_channel=config['fft_params']['dual_channel']),
-                                   transforms.ToTensor(),
-                                   transforms.Normalize(mean=[config['stats']['train_fft_mean']], std=[config['stats']['train_fft_std']])])
 
-    # 2. Datasets
+    val_transform = transforms.Compose([
+        FftTransform(
+        width=config['fft_params']['notch_width'], 
+        notch_depth=config['fft_params']['notch_depth'],
+        apply_bilateral=config['fft_params']['apply_bilateral'],
+        dual_channel=config['fft_params']['dual_channel'] ),
+        v2.Normalize(
+        mean=[config['stats']['train_original_mean'], config['stats']['train_fft_mean']],
+        std=[config['stats']['train_original_std'], config['stats']['train_fft_std']])])
+
+    # 2 Datasets
     if project_root is None:
         project_root = Path.cwd()
     train_ds = DatasetMaker(config['data_path']['train'], transforms=train_transform, project_root=project_root)
     val_ds = DatasetMaker(config['data_path']['val'], transforms=val_transform, project_root=project_root)
     test_ds = DatasetMaker(config['data_path']['test'], transforms=val_transform, project_root=project_root) 
 
-    # 3. Sampler (Weighted to handle imbalanced Solar Fault classes)
+    # 3 Sampler (Weighted to handle imbalancedd classes)
     sample_weights = [config['class_weight'][label] for label in train_ds.data['label']]
     sampler = WeightedRandomSampler(weights=sample_weights, num_samples=len(train_ds), replacement=True)
 
-    # 4. Loaders
+    # 4 loaders
     train_loader = DataLoader(train_ds, batch_size=config['batch_size'], sampler=sampler)
     val_loader = DataLoader(val_ds, batch_size=config['batch_size'])
     test_loader=DataLoader(test_ds,batch_size=config['batch_size'])
